984489ea7d9dde1bd1e55ebca2e417628bae8760
rporopat <poropat.roberto@gmail.com>
2019-05-28 09:20:30 +0200

    Updates

---
+++ precache-manifest.4b9468ff09679424c1d9c1a421768065.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38822b644fb0d1d0654dc3e6778ce8d6",
+    "url": "/Speech2Text/index.html"
+  },
+  {
+    "revision": "78d0029f99b9c92d70d0",
+    "url": "/Speech2Text/static/css/2.e40df323.chunk.css"
+  },
+  {
+    "revision": "5dbc244444d34c348455",
+    "url": "/Speech2Text/static/css/main.6e1afa75.chunk.css"
+  },
+  {
+    "revision": "78d0029f99b9c92d70d0",
+    "url": "/Speech2Text/static/js/2.659c4473.chunk.js"
+  },
+  {
+    "revision": "5dbc244444d34c348455",
+    "url": "/Speech2Text/static/js/main.b4c85df6.chunk.js"
+  },
+  {
+    "revision": "6f2fb0542360fd17a1f8",
+    "url": "/Speech2Text/static/js/runtime~main.3071a92a.js"
+  },
+  {
+    "revision": "6d28c665bf357cadf8a4b4fdbc49d7eb",
+    "url": "/Speech2Text/static/media/a.6d28c665.png"
+  },
+  {
+    "revision": "61bbc30d3e0220f51da442f9ae20346a",
+    "url": "/Speech2Text/static/media/b.61bbc30d.png"
+  },
+  {
+    "revision": "413af02770e2b5eed891a67fa81b99d3",
+    "url": "/Speech2Text/static/media/c.413af027.png"
+  }
+]);
